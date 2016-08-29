318e90f17ac21c41c765caff3222ba8f4e9d5f1a
davidjbrewer <davidjbrewer@eupraxialabs.com>
2016-08-29 21:03:09 +0000

    sprockets fixes

--- app/assets/javascripts/custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.navbar .dropdown').hover(function() {
-        $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideDown();
-    }, function() {
-        $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
-    });
-})
